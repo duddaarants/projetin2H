0ee9fb5f6bfc609eaf6fe4bdcbf29eea692343e9
Maria Eduarda Arantes <88353556+duddaarants@users.noreply.github.com>
2021-09-09 21:26:05 -0300

    Add files via upload

---
+++ expressao_while.php
@@ -0,0 +1,30 @@
+<?php 
+
+$n = 0;
+
+while ($n <=10){
+	echo "<br/>".$n;
+	$n++;
+}
+
+ ?>
+
+
+ <?php 
+
+ $m = 100;
+ while ($m > 0){
+	echo "&nbsp;&nbsp;".$m;
+	$m--;
+
+  ?>
+
+
+
+  <?php 
+
+for ($x=0; $x <= 10 ; $x++) {
+
+echo "<br/>".$x;
+ }
+   ?>

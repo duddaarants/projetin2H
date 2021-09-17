c81767158975ad556bab14959260c6eb42062b4b
Maria Eduarda Arantes <88353556+duddaarants@users.noreply.github.com>
2021-09-16 21:57:03 -0300

    Add files via upload

---
+++ quarta.php
@@ -0,0 +1,15 @@
+<?php 
+
+$valor = 100;
+$valor*9;
+echo "O desconto foi ".$valor."<br/>";
+ ?>
+
+ <?php 
+ //atividade 5
+$valorhora = 7;
+$naulas = 6;
+
+echo "O valor é ".$valorhora*10;
+
+  ?>
